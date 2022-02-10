3391f1838abab704a01a13202aa0440c5b7f00f6
dev <dev@rabins-iMac.local>
2022-02-10 11:32:01 -0800

    let and const

---
+++ index.js
@@ -0,0 +1,20 @@
+let name = 'Rabin';
+console.log(name);
+
+//Rules
+// Cannot be a reserved keyword
+// should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen(-)
+
+
+let firstName= 'Rabin';
+lastName = 'Prajapati';
+
+// Constants
+
+const interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate);
+
+// here if we need reassign use let otherwise use const.
